18d7e6c9e7e51d3880bdc54682e65eeb393d0a97
bakapiano <9417325+bakapiano@user.noreply.gitee.com>
2023-08-11 08:45:02 +0800

    Add try catch

--- src/worker.ts
+++ src/worker.ts
@@ -12,22 +12,27 @@ const configureWorker = () => {
   // Every 2 second
   getWork.second = [...Array.from({ length: 60 }, (_, i) => i)].filter(i => i % 2 === 0);
   schedule.scheduleJob(getWork, async () => {
-    const url = `${config.worker.task}?token=${config.authToken}`;
-    let res = await fetch(url);
-    if (res.status !== 400) {
-      const { data, uuid, type } = (await res.json()) as Task;
-      console.log("[Worker] Get task", res.status, uuid, type)
-      const url = `${config.worker.task}${uuid}/?token=${config.authToken}`;
-      res = await fetch(url, { method: "post" });
-      if (res.status === 200) {
-        console.log("[Worker] Start task", res.status, uuid)
-        const { username, password, authUrl, diffList, traceUUID, pageInfo } = data;
-        const func = type === GameType.maimai ? updateMaimaiScore : updateChunithmScore;
-        func(username, password, authUrl, traceUUID, diffList, pageInfo);
+    try {
+      const url = `${config.worker.task}?token=${config.authToken}`;
+      let res = await fetch(url);
+      if (res.status !== 400) {
+        const { data, uuid, type } = (await res.json()) as Task;
+        console.log("[Worker] Get task", res.status, uuid, type)
+        const url = `${config.worker.task}${uuid}/?token=${config.authToken}`;
+        res = await fetch(url, { method: "post" });
+        if (res.status === 200) {
+          console.log("[Worker] Start task", res.status, uuid)
+          const { username, password, authUrl, diffList, traceUUID, pageInfo } = data;
+          const func = type === GameType.maimai ? updateMaimaiScore : updateChunithmScore;
+          func(username, password, authUrl, traceUUID, diffList, pageInfo).catch(console.log);
+        }
+        else {
+          console.log("[Worker] Failed to ack task", res.status, uuid)
+        }
       }
-      else {
-        console.log("[Worker] Failed to ack task", res.status, uuid)
-      }
+    }
+    catch(err) {
+      console.log("[Worker] Failed when get task", err)
     }
   });
 };
