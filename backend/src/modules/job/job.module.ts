f5bb7072a324334e6e5fba30d600c1856f96c884
bakapiano <9417325+bakapiano@user.noreply.gitee.com>
2026-01-24 23:35:08 +0800

    fix build issue

--- backend/src/modules/job/job.module.ts
+++ backend/src/modules/job/job.module.ts
@@ -1,6 +1,7 @@
 import { JobEntity, JobSchema } from './job.schema';
 import { Module, forwardRef } from '@nestjs/common';
 
+import { AuthModule } from '../auth/auth.module';
 import { JobController } from './job.controller';
 import { JobService } from './job.service';
 import { MongooseModule } from '@nestjs/mongoose';
@@ -10,6 +11,7 @@ import { SyncModule } from '../sync/sync.module';
   imports: [
     MongooseModule.forFeature([{ name: JobEntity.name, schema: JobSchema }]),
     forwardRef(() => SyncModule),
+    forwardRef(() => AuthModule),
   ],
   controllers: [JobController],
   providers: [JobService],
